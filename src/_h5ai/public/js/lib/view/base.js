1310f8da6f8f9a38222fa5f68d091006df4b9e8d
bipin.soni <bipin.soni@guavus.com>
2018-02-01 15:55:38 +0530

    changed top bar with new theme

--- src/_h5ai/public/js/lib/view/base.js
+++ src/_h5ai/public/js/lib/view/base.js
@@ -1,17 +1,15 @@
-const {dom} = require('../util');
+const { dom } = require('../util');
 
 const rootSelector = 'body';
 const topbarTpl =
-        `<div id="topbar">
+    `<div id="topbar">
             <div id="toolbar"></div>
             <div id="flowbar"></div>
-            <a id="backlink" href="https://larsjung.de/h5ai/" title="powered by h5ai - https://larsjung.de/h5ai/">
-                <div>powered</div>
-                <div>by h5ai</div>
-            </a>
+            <div id="logo" style="padding-top: 16px; order: 3; padding-right: 33px;">
+            <img src="/_h5ai/public/images/ui/guavus_logo.svg" alt=">" style=""></div>
         </div>`;
 const mainrowTpl =
-        `<div id="mainrow">
+    `<div id="mainrow">
             <div id="content"></div>
         </div>`;
 
